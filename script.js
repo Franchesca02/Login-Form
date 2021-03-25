a4c86a08cf8db52f79d72dd6acf255d43e0640f3
Franchesca02 <ihuoma.franchesca@gmail.com>
2021-03-25 11:33:28 +0100

    Edited style sheet and javascript

--- script.js
+++ script.js
@@ -1,35 +1,18 @@
-// loop through Array of objects
-
-var objPeople = [
-    { // object @ 0 index
-        username: "uche",
-        password: "chezzy"
-    },
-    { // object @ 1 index
-        username: "uriel",
-        password: "uriely"
-    },
-    { // object @ 2 index
-        username: "jude",
-        password: "judge"
-    }
-
-]
-
+const expextedUsername = "ify20";
+const expextedPassword = "123456";
 function getInfo() {
     var username = document.getElementById('username').value
     var password = document.getElementById('password').value
 
-    for(var i = 0; i < objPeople.length; i++) {
-
-        // check if user input matches username and password of the current index on the objpeople Array
-        if(username == objPeople[i].username && password == objPeople[i].password) {
-            
+        if(username === expextedUsername && password == expextedPassword) {
+            alert("User Login Succesfully")
             console.log(username + "is correct!!!")
             
             // end the function if this comes out true
             return
         }
-    }
+    // }
     console.log("incorrect username or password")
+    alert("incorrect username or password")
+
 }
